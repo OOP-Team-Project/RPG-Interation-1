fec97f775d343ae9e1177f4c510277c0d64a13ad
AndyZhu <zhuyidiwow@gmail.com>
2016-02-02 19:45:55 -0500

    Stats and Occupation
    Stats and Occupation class completed.
    Main.java can be used for testing

---
+++ src/com/oop1/Entity/Smasher.java
@@ -0,0 +1,16 @@
+public class Smasher extends Occupation {
+
+    public Smasher() {
+        baseStrength = 25;
+        strengthIncrement = 4;
+        baseAgility = 20;
+        agilityIncrement = 3;
+        baseIntellect = 15;
+        intellectBase = 2;
+        movementSpeed = 2.5;
+    }
+
+    public void PerformAbility() {
+        System.out.println("Hi I'm Smasher");
+    }
+}

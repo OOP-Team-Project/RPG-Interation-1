08da186eb277b1f75a9154391d85bf0d6528c992
kulakowskin <kulakowskin@yahoo.com>
2016-02-04 13:53:14 -0500

    Added StringStatusView.java and changed MainMenu.java to MainMenuView.java

---
+++ src/com/oop1/View/StringStatusView.java
@@ -0,0 +1,12 @@
+package com.oop1.view;
+
+import java.awt.*;
+
+public class StringStatusView extends View {
+
+    @Override
+    public void draw(Graphics g) {
+        // TODO: implement this
+    }
+
+}

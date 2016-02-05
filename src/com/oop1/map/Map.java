558db7b8ff53487cb01a4c2352a81672d04bfb90
Joshua Nassar <joshnassar94@gmail.com>
2016-02-05 00:13:04 -0500

    "Allows you to set the tiles for the map and to print out the map to a string"

--- src/com/oop1/map/Map.java
+++ src/com/oop1/map/Map.java
@@ -7,6 +7,10 @@ public class Map {
     private Tile[][] tiles;
     private List<AreaEffect> areaEffects;
 
+    public void setTiles(Tile[][] newTiles){
+        tiles = newTiles;
+    }
+
     public static Map generateMap() {
         return new Map();
     }
@@ -18,5 +22,16 @@ public class Map {
                 return tiles[x][y];
         }
         return null;
+    }
+
+    public String toString(){
+        StringBuilder str = new StringBuilder();
+        for(int i = 0; i < tiles.length; ++i){
+            for(int j = 0; j < tiles[i].length; ++j){
+                str.append(tiles[i][j]);
+            }
+            str.append("\n");
+        }
+        return str.toString();
     }
 }

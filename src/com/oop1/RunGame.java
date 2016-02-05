ece4afe2afa3fec18ab1fe6c20293d3a2972573b
kulakowskin <kulakowskin@yahoo.com>
2016-02-05 02:11:47 -0500

    Working on loading decals from file

--- src/com/oop1/RunGame.java
+++ src/com/oop1/RunGame.java
@@ -1,19 +1,30 @@
 package com.oop1;
 import com.oop1.view.AreaView;
-
+import com.oop1.view.DecalView;
 import javax.swing.*;
+import java.awt.*;
 import java.util.Scanner;
 
 public class RunGame {
 
       //private AreaView displayArea = new AreaView();
+     private Graphics2D g;
+
+    public void draw(){
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                AreaView displayArea = new AreaView();
+                (displayArea.gameFrame).setVisible(true);
+                //DecalView dv = new DecalView();
+                //dv.draw(g);
+            }
+        });
+    }
 
       public static void main(String[] args) {
-            SwingUtilities.invokeLater(new Runnable() {
-                public void run() {
-                    AreaView displayArea = new AreaView();
-                    (displayArea.gameFrame).setVisible(true);
-                }
-            });
+
+          RunGame RG = new RunGame();
+          RG.draw();
+
     }
 }
--- src/com/oop1/view/AreaView.java
+++ src/com/oop1/view/AreaView.java
@@ -2,6 +2,7 @@ package com.oop1.view;
 
 import javax.swing.JPanel;
 import javax.swing.JFrame;
+import java.awt.Graphics2D;
 import javax.swing.SwingUtilities;
 import java.awt.GridLayout;
 
@@ -9,15 +10,6 @@ import java.awt.GridLayout;
  * Displays the subset of visible tiles to the player
  */
 public class AreaView extends JPanel {
-
-
-	public AreaView() {
-		setLayout(new GridLayout(0, 20)); //lays the tiles from left to right
-		for (int i = 0; i < 200; i++) {
-			add(new TileView());
-		}
-	}
-
 
 	public JFrame gameFrame = new JFrame();
 	String[] areaEffect = {"grass", "water", "mountains"}; //using list of areaEffect for testing purposes

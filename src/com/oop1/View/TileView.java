6d98f5cd688af9a3d93695fd7a64492d1e9e5dbb
muletmiles <muletmiles@ufl.edu>
2016-02-05 19:10:05 -0500

    Lots more stuff. Still broken

--- src/com/oop1/View/TileView.java
+++ src/com/oop1/View/TileView.java
@@ -4,7 +4,7 @@ import com.oop1.map.Decal;
 import com.oop1.map.Tile;
 
 import javax.imageio.ImageIO;
-import javax.swing.JPanel;
+import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -14,18 +14,31 @@ public class TileView extends JPanel {
 
 	private Tile theTile;	//This is the tile that this view is responsible for viewing
 	private DecalView decalView;
+	public JLabel theLabel;
 
+	private String tileTerrain;
 
 	public TileView(Tile newTile){
 		theTile = newTile;
 
 		if(theTile.hasItem()){
-			add(new ItemView(theTile.getItem()));
+			//add(new ItemView(theTile.getItem()));
+			int i = 0;
 		}
 
 		if(theTile.hasDecal()){
 			add(new DecalView());
 		}
+
+		theLabel = new JLabel("!!!");
+		add(theLabel);
+	}
+
+	public TileView(boolean thing){	//Used to do blank tils.
+		tileTerrain = "Blank";
+
+		theLabel = new JLabel("!!!");
+		add(theLabel);
 	}
 
 	public void drawTile() { //pass Tile tile to this method
@@ -38,7 +51,22 @@ public class TileView extends JPanel {
 		super.paintComponent(g);
 		setLayout(new GridLayout(0, 1));
 
-		g.drawRect(0, 0, 60, 60);
+		//g.drawRect(0, 0, 60, 60);
+		if(theTile != null)
+			tileTerrain = theTile.toString();
+		if (tileTerrain == "M") {
+			g.setColor(Color.gray);
+		} else if (tileTerrain == "G") {
+			g.setColor(Color.green);
+		} else if (tileTerrain == "W"){
+			g.setColor(Color.blue);
+		} else{
+			g.setColor(Color.black);
+		}
+
+
+		this.setBorder(BorderFactory.createLineBorder(Color.black));
+		g.fillRect(0, 0, 60, 60);
 	}
 	
 }
--- src/com/oop1/map/Map.java
+++ src/com/oop1/map/Map.java
@@ -35,11 +35,32 @@ public class Map {
         return str.toString();
     }
 
-    public int getXBoundary(){
+    public int getXBoundary() {
         return tiles.length;
     }
-
     public int getYBoundary(){
         return tiles[0].length;
+    }
+
+    public int findXLocation(Tile toFind){
+
+
+        for(int i = 0; i < tiles.length; i++){
+            for(int j = 0; j < tiles[i].length; j++){
+                if(tiles[i][j] == toFind)
+                    return i;
+            }
+        }
+        return -1;  //Couldn't find it!
+    }
+
+    public int findYLocation(Tile toFind){
+        for(int i = 0; i < tiles.length; i++){
+            for(int j = 0; j < tiles[i].length; j++){
+                if(tiles[i][j] == toFind)
+                    return j;
+            }
+        }
+        return -1;  //Couldn't find it!
     }
 }

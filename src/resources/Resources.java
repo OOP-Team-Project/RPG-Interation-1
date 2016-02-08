d8d60c5a4947e258b5749e72a80e5fdbf2a37a8c
Josh <joshnassar94@gmail.com>
2016-02-07 22:25:18 -0500

    Game works when building from source now, except for saving...

---
+++ src/resources/Resources.java
@@ -0,0 +1,11 @@
+package com.oop1.resources;
+
+import javax.swing.*;
+
+/**
+ * Created by Miles on 2/7/16.
+ */
+public class Resources {    //Holds a bunch of buffered images (so we don't have to keep reloading them.
+    public static ImageIcon SWORD_ICON = new ImageIcon("src/com/oop1/resources/TAKEABLE.png");
+
+}
